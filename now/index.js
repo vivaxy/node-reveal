861fe249dd81b02ed5010ee77462f917f5716e9a
vivaxy <xyxuye2007@126.com>
2019-06-18 13:50:33 +0800

    ci(now): :construction_worker: Add now.sh

---
+++ now/index.js
@@ -0,0 +1,22 @@
+/**
+ * @since 2019-06-18 13:46
+ * @author vivaxy
+ */
+const { handler, builder } = require('../commands/server.js');
+
+(async () => {
+  const args = {};
+  Object.keys(builder).forEach((key) => {
+    if (builder[key].hasOwnProperty('default')) {
+      args[key] = builder[key].default;
+    }
+  });
+
+  await handler({
+    ...args,
+    markdown: 'test/demo.md',
+    watch: true,
+    logLevel: 0,
+    script: 'custom.js',
+  });
+})();
